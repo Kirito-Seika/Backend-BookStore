d83f49386b638b96fadc3f54c70413723f865c92
Kirito-Seika <nguyenminhduc.developer@gmail.com>
2025-03-13 05:43:18 +0700

    Validator User

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,20 @@
+import { Validate } from 'class-validator';
+import {
+  IsEmailValidConstraint,
+  IsNameValidConstraint,
+  IsPasswordValidConstraint, IsPhoneValidConstraint,
+} from '../validator/validator.customize';
+
 export class CreateUserDto {
+  @Validate(IsNameValidConstraint)
   fullName: string;
+
+  @Validate(IsEmailValidConstraint)
   email: string;
+
+  @Validate(IsPasswordValidConstraint)
   password: string;
+
+  @Validate(IsPhoneValidConstraint)
   phone: string;
 }
